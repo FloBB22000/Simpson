d4e186e4f510d5653e8c6b71ba52e249f3ce6e14
FloBB22000 <florence.goslino@sncf.fr>
2022-06-23 16:38:07 +0200

    application simpson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import SimpsonPersonnage from './components/SimpsonPersonnage.js';
+import axios from 'axios';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const samplePerson = {
+    "quote": "By chilling my loins I increase the chances of impregnating my wife.",
+    "character": "Apu Nahasapeemapetilon",
+    "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+    "characterDirection": "Left"
+    }
 
-export default App;
+    function App() {
+      const [person, setPerson] = useState(samplePerson);
+      const getPerson = () => {
+        // Send the request
+        axios
+          .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+          // Extract the DATA from the received response
+          .then((response) => response.data)
+          // Use this data to update the state
+          .then((data) => {
+          setPerson(data[0]);
+          });
+      };
+      return (
+        <div className='App'>
+        <SimpsonPersonnage person={person} />
+        <button type="button" onClick={getPerson}>Get Person</button>
+        </div>
+      );
+    }
+    
+    export default App;
---
+++ src/components/SimpsonPersonnage.js
@@ -0,0 +1,18 @@
+// src/components/DisplayEmployee.jsx
+import React from 'react';
+
+function SimpsonPersonnage({person}) {
+  return (
+    person && (
+      <div className='SimpsonPersonnage'>
+        <img src={person.image} alt={person.character} />
+        <ul>
+          <li>Quote {person.quote}</li>
+          <li>Name: {person.character} </li>
+        </ul>
+      </div>
+    )
+  );
+}
+
+export default SimpsonPersonnage;
